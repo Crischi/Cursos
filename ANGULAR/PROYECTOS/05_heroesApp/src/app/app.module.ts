5f1774e1e615ff8cd38cb2352cdf0c2cf8614e8d
Cristina <cristinachiarri@gmail.com>
2022-08-08 17:11:24 +0200

    backend server, Postman, interface & Angular flex & pipe image

--- ANGULAR/PROYECTOS/05_heroesApp/src/app/app.module.ts
+++ ANGULAR/PROYECTOS/05_heroesApp/src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
+
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
 
 //MODULES
 import { AppRoutingModule } from './app-routing.module';
@@ -18,6 +20,7 @@ import { ErrorPageComponent } from './shared/error-page/error-page.component';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    HttpClientModule,
     AppRoutingModule
   ],
   providers: [],
--- ANGULAR/PROYECTOS/05_heroesApp/src/app/heroes/pages/listado/listado.component.ts
+++ ANGULAR/PROYECTOS/05_heroesApp/src/app/heroes/pages/listado/listado.component.ts
@@ -1,16 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { HeroesService } from '../../services/heroes.service';
+import { ResultadosComponent } from '../../../../../../02-gifsApp/src/app/gifs/resultados/resultados.component';
+import { Heroe } from '../../interfaces/heroes.interface';
 
 @Component({
   selector: 'app-listado',
   templateUrl: './listado.component.html',
   styles: [
-  ]
+    `
+      mat-card {
+        margin-top: 20px;
+      }
+    `,
+  ],
 })
 export class ListadoComponent implements OnInit {
+  heroes: Heroe[] = [];
 
-  constructor() { }
+  constructor(private heroesService: HeroesService) {}
 
   ngOnInit(): void {
+    this.heroesService.getHeroes().subscribe((resp) => (this.heroes = resp));
   }
-
 }
