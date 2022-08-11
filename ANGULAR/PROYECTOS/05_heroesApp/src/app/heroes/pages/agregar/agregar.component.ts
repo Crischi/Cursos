87ed309a4355ee9f2f1d01e9f3395d74955cc0ad
Cristina <cristinachiarri@gmail.com>
2022-08-11 13:42:42 +0200

    CRUD:post & put

--- ANGULAR/PROYECTOS/05_heroesApp/src/app/heroes/pages/agregar/agregar.component.ts
+++ ANGULAR/PROYECTOS/05_heroesApp/src/app/heroes/pages/agregar/agregar.component.ts
@@ -1,16 +1,55 @@
 import { Component, OnInit } from '@angular/core';
+import { Heroe, Publisher } from '../../interfaces/heroes.interface';
+import { HeroesService } from '../../services/heroes.service';
+import { ActivatedRoute } from '@angular/router';
+import { switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-agregar',
   templateUrl: './agregar.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class AgregarComponent implements OnInit {
+  publishers = [
+    { id: 'DC Comics', desc: 'DC - Comics' },
+    { id: 'Marvel Comics', desc: 'Marvel - Comics' },
+  ];
 
-  constructor() { }
+  heroe: Heroe = {
+    superhero: '',
+    alter_ego: '',
+    characters: '',
+    first_appearance: '',
+    publisher: Publisher.DCComics,
+    alt_img: '',
+    //id no obligatorio
+  };
+
+  constructor(
+    private heroesService: HeroesService,
+    private activatedRoute: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
+    //al cambiar la url (editar heroe) abre formulario volcando sus datos
+    this.activatedRoute.params
+      .pipe(switchMap(({ id }) => this.heroesService.getHeroesId(id)))
+      .subscribe((heroe) => (this.heroe = heroe));
   }
 
+  guardarDatosHeroe() {
+    if (this.heroe.superhero.trim().length === 0) {return;}
+    //Diferenciamos editar de añadir
+    if (this.heroe.id) {
+      //Actualizar
+      this.heroesService
+        .actualizarHeroe(this.heroe)
+        .subscribe((heroe) => console.log('Actualizando...', heroe));
+    } else {
+      //Crear   this.heroesService
+      this.heroesService
+        .agregarHeroe(this.heroe)
+        .subscribe((resp) => console.log('Respuesta:', resp));
+    }
+  }
 }
