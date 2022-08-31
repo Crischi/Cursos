10e975a56c6bba2a34d411e8f1c4d1f58c4869c1
Cristina <cristinachiarri@gmail.com>
2022-08-31 13:08:17 +0200

    Apuntes review

--- ANGULAR/PROYECTOS/05_heroesApp/src/app/heroes/pages/home/home.component.ts
+++ ANGULAR/PROYECTOS/05_heroesApp/src/app/heroes/pages/home/home.component.ts
@@ -1,19 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styles: [
-    `
-      .container {
+    ` .container {
         margin: 10px;
       }
-      
     `,
   ],
 })
-export class HomeComponent implements OnInit {
-  constructor() {}
+export class HomeComponent {
+  constructor(private router: Router) {}
 
-  ngOnInit(): void {}
+  logout() {
+    this.router.navigate(['./auth']);
+  }
 }
